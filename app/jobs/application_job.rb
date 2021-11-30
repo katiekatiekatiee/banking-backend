c2bc70ee94ea392a4acf0890ff960644df2e872b
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-11-29 22:53:24 -0500

    test commit

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -5,3 +5,5 @@ class ApplicationJob < ActiveJob::Base
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
 end
+
+#xyz
