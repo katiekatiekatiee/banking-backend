656f4bdb836ec1220a51bd049cb8c4336a37dfca
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-11-30 23:25:17 -0500

    filled out transactions controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,18 +2,28 @@ class TransactionsController < ApplicationController
 
     def index
         transactions = Transaction.all 
-
+        render json: transactions
     end
 
     def create
-        transaction = Transaction.new
+        transaction = Transaction.create(transaction_params)
+
+        if transaction.save
+            render json: transaction
+        else 
+            render json: {error: "Could not save transaction data."}
+        end
     end
 
     def show
         transaction = Transaction.find(params[:id])
+        render json: transaction.to_json(except: [:created_at, :updated_at])
     end
 
     def destroy
+        transaction = Transaction.find(params[:id])
+        transaction.destroy
+        render json: {message: "Transaction has been removed."}
     end
 
     private
