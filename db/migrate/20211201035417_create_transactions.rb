2b7a3de8b25ebb3b2b54eb22a2c9fe9b2923f561
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-11-30 23:38:18 -0500

    database established

--- db/migrate/20211201035417_create_transactions.rb
+++ db/migrate/20211201035417_create_transactions.rb
@@ -1,10 +1,11 @@
 class CreateTransactions < ActiveRecord::Migration[6.1]
   def change
     create_table :transactions do |t|
-      t.integer :id
-      t.integer :goal_id
+      # t.integer :id
+      # t.integer :goal_id
       t.string :date
       t.string :amount
+      t.belongs_to :goal
 
       t.timestamps
     end
--- db/migrate/20211201035452_create_goals.rb
+++ db/migrate/20211201035452_create_goals.rb
@@ -1,7 +1,7 @@
 class CreateGoals < ActiveRecord::Migration[6.1]
   def change
     create_table :goals do |t|
-      t.integer :id
+      # t.integer :id
       t.string :name
 
       t.timestamps
