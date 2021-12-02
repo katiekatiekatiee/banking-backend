1b08418cd4db6ecb1ca1f9a7c706af15a33f0319
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-12-01 19:44:40 -0500

    seeded transaction data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,53 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Goal.create(name: "Shopping")
-Goal.create(name: "Vacation")
-Goal.create(name: "Holiday Gifts")
-Goal.create(name: "Personal")
-Goal.create(name: "General Savings")
-Goal.create(name: "Vehicle")
-Goal.create(name: "Home")
-Goal.create(name: "Retirement")
-Goal.create(name: "School")
-Goal.create(name: "Concerts")
+# Goal.create(name: "Shopping")
+# Goal.create(name: "Vacation")
+# Goal.create(name: "Holiday Gifts")
+# Goal.create(name: "Personal")
+# Goal.create(name: "General Savings")
+# Goal.create(name: "Vehicle")
+# Goal.create(name: "Home")
+# Goal.create(name: "Retirement")
+# Goal.create(name: "School")
+# Goal.create(name: "Concerts")
+
+Transaction.create(date: "12/1/21", amount: "$40", goal_id: 1)
+Transaction.create(date: "11/11/21", amount: "$90", goal_id: 1)
+Transaction.create(date: "12/2/21", amount: "$25", goal_id: 1)
+
+Transaction.create(date: "12/1/21", amount: "$40", goal_id: 2)
+Transaction.create(date: "11/11/21", amount: "$90", goal_id: 2)
+Transaction.create(date: "12/2/21", amount: "$25", goal_id: 2)
+
+Transaction.create(date: "12/1/21", amount: "$40", goal_id: 3)
+Transaction.create(date: "11/11/21", amount: "$90", goal_id: 3)
+Transaction.create(date: "12/2/21", amount: "$25", goal_id: 3)
+
+Transaction.create(date: "12/1/21", amount: "$40", goal_id: 4)
+Transaction.create(date: "11/11/21", amount: "$90", goal_id: 4)
+Transaction.create(date: "12/2/21", amount: "$25", goal_id: 4)
+
+Transaction.create(date: "12/1/21", amount: "$40", goal_id: 5)
+Transaction.create(date: "11/11/21", amount: "$90", goal_id: 5)
+Transaction.create(date: "12/2/21", amount: "$25", goal_id: 5)
+
+Transaction.create(date: "12/1/21", amount: "$40", goal_id: 6)
+Transaction.create(date: "11/11/21", amount: "$90", goal_id: 6)
+Transaction.create(date: "12/2/21", amount: "$25", goal_id: 6)
+
+Transaction.create(date: "12/1/21", amount: "$40", goal_id: 7)
+Transaction.create(date: "11/11/21", amount: "$90", goal_id: 7)
+Transaction.create(date: "12/2/21", amount: "$25", goal_id: 7)
+
+Transaction.create(date: "12/1/21", amount: "$40", goal_id: 8)
+Transaction.create(date: "11/11/21", amount: "$90", goal_id: 8)
+Transaction.create(date: "12/2/21", amount: "$25", goal_id: 8)
+
+Transaction.create(date: "12/1/21", amount: "$40", goal_id: 9)
+Transaction.create(date: "11/11/21", amount: "$90", goal_id: 9)
+Transaction.create(date: "12/2/21", amount: "$25", goal_id: 9)
+
+Transaction.create(date: "12/1/21", amount: "$40", goal_id: 10)
+Transaction.create(date: "11/11/21", amount: "$90", goal_id: 10)
+Transaction.create(date: "12/2/21", amount: "$25", goal_id: 10)
